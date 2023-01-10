4f72c0863365d4bd65243751fb4b9efea44dc53e
tvziet <vietleuit@gmail.com>
2023-01-10 15:44:21 +0700

    Create a seed data of user, and product

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+User.delete_all
+
+5.times do
+  user = User.create!(email: Faker::Internet.email, password: "p@ssw0rd")
+  p "Created a new user with email: #{user.email}"
+
+  2.times do
+    product = Product.create!(title: Faker::Commerce.product_name,
+                              price: rand(1.0..100.0),
+                              published: true,
+                              user_id: user.id)
+    p "Created a brand new product #{product.title}"
+  end
+end
