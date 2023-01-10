553113d16cc74fda25273ab5705daba714a4b1dd
tvziet <vietleuit@gmail.com>
2023-01-10 09:00:31 +0700

    Restrict actions for unauthorized users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
+  before_action :check_owner, only: %i[update destroy]
 
   def show
     render json: @user
@@ -34,6 +35,10 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def check_owner
+    head :forbidden unless @user.id = @current_user&.id
   end
 end
 
