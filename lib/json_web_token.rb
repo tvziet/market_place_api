2a230521fbe68c151d8c9c2df52626da4a8b9196
Viet Le <vietleuit@gmail.com>
2024-08-25 06:30:19 +0700

    fix: CI failed

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,11 +1,11 @@
 class JsonWebToken
   def self.encode(payload, exp = 1.hour.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, ENV['SECRET_KEY'])
+    JWT.encode(payload, ENV.fetch('SECRET_KEY'))
   end
 
   def self.decode(token)
-    decoded = JWT.decode(token, ENV['SECRET_KEY']).first
+    decoded = JWT.decode(token, ENV.fetch('SECRET_KEY')).first
     ActiveSupport::HashWithIndifferentAccess.new(decoded)
   end
 end
